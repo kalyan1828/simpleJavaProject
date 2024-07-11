4334ad3ae2f08653e7e35f5a50433214e391b719
Kalyan Goud <kalyangoud1828@gmail.com>
2024-07-11 20:59:46 +0530

    First Commit

---
+++ src/example/First.java
@@ -0,0 +1,16 @@
+package example;
+import entity.*;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		Username us=new Username();
+		us.setId(100);
+		int n= us.getId();
+		System.out.println(n);
+		
+	}
+
+}
